7c7fdfffa2f7c34a3a0da28028bad1f8497840ef
Godwin Pang <gpang@confluent.io>
2020-11-05 13:06:36 -0800

    envvar

--- bot.js
+++ bot.js
@@ -24,12 +24,7 @@ if (process.env.MONGO_URI) {
     });
 }
 
-
-
 const adapter = new SlackAdapter({
-    // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
-    enable_incomplete: true,
-
     // parameters used to secure webhook endpoint
     verificationToken: process.env.VERIFICATION_TOKEN,
     clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
